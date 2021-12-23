269d843f8aea72c8b1064fa131faf6066d79015f
Thiago Negherbon <thiagonegherbon@outlook.com>
2021-12-22 23:08:26 -0300

    - Solution and project creation; - Solution class creation;

---
+++ Leet-Code-Problems/Two-Sum-Project/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum_Project
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int index1 = 0, index2 = 1;
+
+            foreach (int number in nums)
+            {
+                if (index2 <= nums.Count())
+                {
+                    if ((nums[index1] + nums[index2]) == target)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        index1++;
+                        index2++;
+                    }
+                }
+            }
+
+            return new int[] { index1, index2 };
+        }
+    }
+}
